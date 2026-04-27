89fb27b7978653bf19086f126ff177e2425d738d
GusiPusi123 <matveykulmanov0809.1219@gmail.com>
2026-04-27 18:32:14 +0300

    update 2

--- GrandmotherInTheVillage/Assets/Player/Scripts/Player.cs
+++ GrandmotherInTheVillage/Assets/Player/Scripts/Player.cs
@@ -1,86 +1,4 @@
-﻿// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.SceneManagement;
-// using UnityEngine.UI; // Для UI элементов
-
-// public class Player : MonoBehaviour
-// {
-//     public float speed = 5;
-
-//     [Header("Running")]
-//     public bool canRun = true;
-//     public bool IsRunning { get; private set; }
-//     public float runSpeed = 9;
-//     public float maxHP = 100f; // Максимальное здоровье
-//     public string SceneDie;
-//     private float currentHP;
-
-//     public KeyCode runningKey = KeyCode.LeftShift;
-
-//     Rigidbody rigidbody;
-
-//     public List<System.Func<float>> speedOverrides = new List<System.Func<float>>();
-
-//     // Для отображения HP на экране
-//     public Text hpText;
-
-//     void Start()
-//     {
-//         currentHP = maxHP;
-//         UpdateHPText(); // Обновляем сразу при старте
-//     }
-
-//     void Awake()
-//     {
-//         rigidbody = GetComponent<Rigidbody>();
-//     }
-
-//     void FixedUpdate()
-//     {
-//         if (currentHP <= 0) return; // Не двигаться, если умер
-
-//         IsRunning = canRun && Input.GetKey(runningKey);
-
-//         float targetMovingSpeed = IsRunning ? runSpeed : speed;
-//         if (speedOverrides.Count > 0)
-//         {
-//             targetMovingSpeed = speedOverrides[speedOverrides.Count - 1]();
-//         }
-
-//         Vector2 targetVelocity = new Vector2(Input.GetAxis("Horizontal") * targetMovingSpeed, Input.GetAxis("Vertical") * targetMovingSpeed);
-
-//         rigidbody.velocity = transform.rotation * new Vector3(targetVelocity.x, rigidbody.velocity.y, targetVelocity.y);
-//     }
-
-//     public void TakeDamage(float amount)
-//     {
-//         if (currentHP <= 0) return;
-
-//         currentHP -= amount;
-//         UpdateHPText();
-
-//         if (currentHP <= 0)
-//         {
-//             Die();
-//         }
-//     }
-
-//     void UpdateHPText()
-//     {
-//         if (hpText != null)
-//         {
-//             hpText.text = "" + Mathf.CeilToInt(currentHP).ToString();
-//         }
-//     }
-
-//     void Die()
-//     {
-//         SceneManager.LoadScene(SceneDie);
-//     }
-// }
-
-
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -95,10 +13,8 @@ public class Player : MonoBehaviour
     public float runSpeed = 9;
     public float maxHP = 100f; // Максимальное здоровье
     public string SceneDie;
-
-    private float currentHP;
-
     public KeyCode runningKey = KeyCode.LeftShift;
+    private float currentHP;
 
     Rigidbody rigidbody;
 

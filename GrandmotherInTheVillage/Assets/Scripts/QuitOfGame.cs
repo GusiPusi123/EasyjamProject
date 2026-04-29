8f1ff2ca59ce49dbfa782c7ae59bf4d20a5c3eb7
GusiPusi123 <matveykulmanov0809.1219@gmail.com>
2026-04-29 09:55:50 +0300

    Update 7

---
+++ GrandmotherInTheVillage/Assets/Scripts/QuitOfGame.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitOfGame : MonoBehaviour
+{
+    public void OnButtonClicked()
+    {
+        Application.Quit();
+    }
+}
